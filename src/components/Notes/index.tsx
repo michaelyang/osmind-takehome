b2114b839e95d5b1ffec1df98e3fcf535cb7067b
Michael Yang <myang1@wayfair.com>
2021-02-22 02:14:03 -0500

    UI portion of search and replace

--- src/components/Notes/index.tsx
+++ src/components/Notes/index.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
+import InputGroup from 'react-bootstrap/InputGroup';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+import FormControl from 'react-bootstrap/FormControl';
+import Collapse from 'react-bootstrap/Collapse';
 import Loading from '../common/Loading';
 import NotesList from './NotesList';
 import { filterNotes } from '../../utils/searchFunctions';
@@ -11,25 +17,72 @@ export interface NotesProps {
   isLoading: boolean;
 }
 
-const Notes: React.FC<NotesProps> = ({ notes, isLoading }) => {
-  const [searchTerm, setSearchTerm] = useState('');
+enum ActionType {
+  Find,
+  FindAndReplace,
+}
 
-  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.target.value);
+const ActionName = new Map<ActionType, string>([
+  [ActionType.Find, 'Find'],
+  [ActionType.FindAndReplace, 'Find & Replace'],
+]);
+
+const Notes: React.FC<NotesProps> = ({ notes, isLoading }) => {
+  const [findTerm, setFindTerm] = useState<string>('');
+  const [replaceWith, setReplaceWith] = useState<string>('');
+  const [actionType, setActionType] = useState<ActionType>(ActionType.Find);
+
+  const handleFindInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFindTerm(e.target.value);
   };
 
-  const filteredNotes = filterNotes(notes, searchTerm);
+  const handleReplceInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setReplaceWith(e.target.value);
+  };
+
+  const filteredNotes = filterNotes(notes, findTerm);
 
   return (
     <div className="notes">
       <h2 className="pb-3 mt-4 mb-3 border-bottom">Your Notes</h2>
-      <div className="search-container">
-        <input
-          type="text"
-          placeholder="Search"
-          value={searchTerm}
-          onChange={handleSearchInputChange}
-        />
+      <div className="search-container pb-3 mb-3 border-bottom">
+        <InputGroup>
+          <FormControl
+            type="text"
+            placeholder="Find"
+            value={findTerm}
+            onChange={handleFindInputChange}
+            tabIndex={2}
+          />
+          <DropdownButton
+            as={InputGroup.Prepend}
+            variant="secondary"
+            title={ActionName.get(actionType)}
+            tabIndex={1}
+          >
+            <Dropdown.Item onClick={() => setActionType(ActionType.Find)}>
+              {ActionName.get(ActionType.Find)}
+            </Dropdown.Item>
+            <Dropdown.Item onClick={() => setActionType(ActionType.FindAndReplace)}>
+              {ActionName.get(ActionType.FindAndReplace)}
+            </Dropdown.Item>
+          </DropdownButton>
+        </InputGroup>
+        <Collapse in={actionType === ActionType.FindAndReplace}>
+          <InputGroup className="mt-2">
+            <FormControl type="text" placeholder="Replace with..." tabIndex={3} />
+            <InputGroup.Append>
+              <Button
+                variant="primary"
+                value={replaceWith}
+                onChange={handleReplceInputChange}
+                tabIndex={4}
+              >
+                Replace
+              </Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Collapse>
       </div>
       <ListGroup>
         <LinkContainer to="/notes/new">
@@ -41,7 +94,7 @@ const Notes: React.FC<NotesProps> = ({ notes, isLoading }) => {
         {isLoading ? (
           <Loading text="Loading Your Notes..." style={{ margin: '20px 0' }} />
         ) : (
-          <NotesList notes={filteredNotes} searchTerm={searchTerm} />
+          <NotesList notes={filteredNotes} searchTerm={findTerm} />
         )}
       </ListGroup>
     </div>
